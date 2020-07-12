12271d1bf26fe2414e78bf7eab28add4110ea8ab
Jugal08 <aboy1342@gmail.com>
2020-07-12 15:52:58 +0530

    Finished Radio Buttons

--- Radio.py
+++ Radio.py
@@ -6,6 +6,21 @@ root=Tk()
 root.geometry("200x200")
 root.title("Learning radio Buttons")
 
+Fruits=StringVar()
+Fruits.set("Apple")
+
+
+MODES=[("Apple","Apple"),
+       ("Banana","Banana"),
+       ("Mango","Mango"),
+       ("Watermelon","Watermelon"),
+       ("Grapes","Grapes")  
+  ]
+
+
+
+for text,modes in MODES:
+    Radiobutton(root,text=text,variable=Fruits,value=modes).pack(anchor=W)
 
 def Clicked(value):
     label=Label(root,text=value)
@@ -13,13 +28,19 @@ def Clicked(value):
 
 
 
+#a=IntVar()
+#a.set(2)
 
-a=IntVar()
-a.set(2)
-Radiobutton(root,text="Option1",variable=a,value=1,command= lambda:Clicked(a.get())).pack()
-Radiobutton(root,text="Option2",variable=a,value=2,command= lambda:Clicked(a.get())).pack()
+#Radiobutton(root,text="Option1",variable=a,value=1,command= lambda:Clicked(a.get())).pack()
 
-label=Label(root,text=a.get())
-label.pack()
+
+#Radiobutton(root,text="Option2",variable=a,value=2,command= lambda:Clicked(a.get())).pack()
+
+button1=Button(root,text="Click me",command=lambda : Clicked(Fruits.get())).pack()
+
+
+
+#label=Label(root,text=Fruits.get())
+#label.pack()
 
 root.mainloop()
